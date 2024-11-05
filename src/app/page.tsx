ef81b3683d92f265282d85188a8ed8e7f6c953c9
Vlad <vladm19082006@gmail.com>
2024-11-05 18:47:54 +0200

    Session middleware is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+
+  }, [data]);
+
   return (
-    <div className="">
-      <Button variant={"primary"} size={"xs"}>Primary</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"teritary"}>Teritary</Button>
+    <div>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
+      </Button>
     </div>
   );
 }
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -6,11 +6,16 @@ import { deleteCookie, setCookie } from "hono/cookie";
 import { zValidator } from "@hono/zod-validator";
 
 import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { AUTH_COOKIE } from "../constants";
 import { loginSchema, registerSchema } from "../schemas";
 
-const app = new Hono().post("/login", zValidator("json", loginSchema), async (c) => {
+const app = new Hono().get("/current", sessionMiddleware, (c) => {
+  const user = c.get("user");
+
+  return c.json({ data: user });
+}).post("/login", zValidator("json", loginSchema), async (c) => {
   const { email, password } = c.req.valid("json");
 
   const { account } = await createAdminClient();
@@ -59,8 +64,12 @@ const app = new Hono().post("/login", zValidator("json", loginSchema), async (c)
     return c.json({ success: true });
   }
 )
-.post("/logout", (c) => {
+.post("/logout", sessionMiddleware, async (c) => {
+  const account = c.get("account");
+
+
   deleteCookie(c, AUTH_COOKIE);
+  await account.deleteSession("current");
 
   return c.json({ success: true });
 });
